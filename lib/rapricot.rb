e373e9fb387df0d270ba638bc2a539a491dc962a
Logan Campbell-McPherson <colin@logaan.net>
2013-01-16 21:46:09 +1100

    switched to module and class << self

--- lib/rapricot.rb
+++ lib/rapricot.rb
@@ -1,35 +1,37 @@
-class Rapricot
-  def self.render(document)
-    case document
-      when Array then render_element(document)
-      else document.to_s
+module Rapricot
+  class << self
+    def render(document)
+      case document
+        when Array then render_element(document)
+        else document.to_s
+      end
     end
-  end
 
-  def self.render_element(element)
-    tag        = element.shift.to_s
-    attributes = render_attributes(element)
-    content    = element.map{|c| render(c) + "\n" }.join
+    def render_element(element)
+      tag        = element.shift.to_s
+      attributes = render_attributes(element)
+      content    = element.map{|c| render(c) + "\n" }.join
 
-    wrap_in_tag(tag, attributes, content)
-  end
+      wrap_in_tag(tag, attributes, content)
+    end
 
-  def self.wrap_in_tag(tag, attributes, content)
-    "<#{tag}#{attributes}>\n" + indent_lines(content) + "</#{tag}>"
-  end
+    def wrap_in_tag(tag, attributes, content)
+      "<#{tag}#{attributes}>\n" + indent_lines(content) + "</#{tag}>"
+    end
 
-  def self.indent_lines(lines)
-    lines.each_line.map{|l| "  " + l }.join
-  end
+    def indent_lines(lines)
+      lines.each_line.map{|l| "  " + l }.join
+    end
 
-  def self.render_attributes(element)
-    case element.first
-    when Hash
-      element.shift.map do |key, value|
-        " #{key}=\"#{value}\""
-      end.join
-    else
-      ""
+    def render_attributes(element)
+      case element.first
+      when Hash
+        element.shift.map do |key, value|
+          " #{key}=\"#{value}\""
+        end.join
+      else
+        ""
+      end
     end
   end
 end

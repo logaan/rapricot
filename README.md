7034bfa94d1d44fef04a306bbfded3957f862aff
Logan Campbell-McPherson <colin@logaan.net>
2013-01-16 12:30:02 +1100

    actually executed the example

--- README.md
+++ README.md
@@ -49,25 +49,25 @@ Will print (all on one line):
 ```ruby
 # Form Helper
 def text_field(name, value)
-  [:input, {type: text, name: name, value: value}]
+  [:input, {type: "text", name: name, value: value}]
 end
 
 # Partial
 def user_partial(user)
   [:div, {class: "user"},
-    [:span, {class: "name"}, user.name]
-    [:ul, {class: "friends"}].concat(user.friends.map |friend|
+    [:span, {class: "name"}, user.name],
+    [:ul, {class: "friends"}].concat(user.friends.map do |friend|
       [:li, {class: "friend"}, friend.name]
     end)]
 end
 
 # Layout
 def default_layout(title, page)
-  [:html
-    [:head
-      [:title title]]
-    [:body
-      [:div, {id: container}].concat(page)]]
+  [:html,
+    [:head,
+      [:title, title]],
+    [:body,
+      [:div, {id: "container"}].concat(page)]]
 end
 
 # Complete view
@@ -77,9 +77,9 @@ end
 
 def user_page(user, search_query)
   Rapricot.render(
-    default_layout("#{user.name}'s page",
+    default_layout("#{user.name}'s page", [
       search_box(search_query),
-      user_partial(user)))
+      user_partial(user)]))
 end
 ```
 
---
+++ example.rb
@@ -0,0 +1,53 @@
+require "./rapricot"
+
+# Form Helper
+def text_field(name, value)
+  [:input, {type: "text", name: name, value: value}]
+end
+
+# Partial
+def user_partial(user)
+  [:div, {class: "user"},
+    [:span, {class: "name"}, user.name],
+    [:ul, {class: "friends"}].concat(user.friends.map do |friend|
+      [:li, {class: "friend"}, friend.name]
+    end)]
+end
+
+# Layout
+def default_layout(title, page)
+  [:html,
+    [:head,
+      [:title, title]],
+    [:body,
+      [:div, {id: "container"}].concat(page)]]
+end
+
+# Complete view
+def search_box(search_query)
+  [:div, {class: "search"}, text_field("search_query", search_query)]
+end
+
+def user_page(user, search_query)
+  Rapricot.render(
+    default_layout("#{user.name}'s page", [
+      search_box(search_query),
+      user_partial(user)]))
+end
+
+# Model
+class User
+  attr_accessor :name, :friends
+
+  def initialize(name, friends)
+    @name = name
+    @friends = friends
+  end
+end
+
+# Data
+andy  = User.new("Andy",  [])
+kiril = User.new("Kiril", [])
+logan = User.new("Logan", [andy, kiril])
+
+puts user_page(logan, "log")

a0f9be6a7903421d5b45885530c83f2ce17d7bd0
Arvind Kunday <hi@kunday.com>
2013-01-17 19:24:15 +1100

    Basic tests for void, regular & nested elements.

---
+++ spec/lib/rapricot_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Rapricot do
+	include Rapricot
+	it "should render void elements" do
+		render([:img, {src: "a.img"}]).should == "<img src=\"a.img\" >"
+	end
+	it "should render normal elements" do
+		render([:html]).gsub("\n","").should == "<html></html>"
+	end
+	it "should render nested elements" do
+		# why are there extra spaces?
+		render([:html, [:head]]).gsub("\n", "").should == "<html>  <head>  </head></html>"
+	end
+	it "should render elements correctly" do
+		#should the escape characters be shown in output?
+		render([:input, {type: "text", name: "email", value: "email"}]).should == "<input type=\"text\" name=\"email\" value=\"email\">"
+	end
+end

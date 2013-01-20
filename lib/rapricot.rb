287aa77072bd0f96afa1dd613329fc73928eb2e2
Logan Campbell-McPherson <colin@logaan.net>
2013-01-20 17:18:27 +1100

    translated into more idiomatic ruby

--- lib/rapricot.rb
+++ lib/rapricot.rb
@@ -1,53 +1,57 @@
-module Rapricot
+class Tag
   VOID_ELEMENTS = %w[area base br col command embed hr img input keygen link
   meta param source track wbr]
 
-  def render(document)
-    case document
-      when Array then render_element(standardise_attributes(document))
-      else document.to_s
-    end
+  def initialize(array_form)
+    @tag, @attributes, *@content = array_form
   end
 
-  def render_element(element)
-    tag        = element.shift.to_s
-    attributes = render_attributes(element)
-
-    if VOID_ELEMENTS.include?(tag)
-      void_tag(tag, attributes)
-    else
-      content    = element.map{|c| render(c) + "\n" }.join
-      wrap_in_tag(tag, attributes, content)
-    end
+  def render
+    void? ? opening_tag : opening_tag + content + closing_tag
+  end
+  
+  def void?
+    VOID_ELEMENTS.include?(@tag.to_s)
   end
 
-  def void_tag(tag, attributes)
-    "<#{tag}#{attributes}>"
+  def opening_tag
+    "<#{@tag}#{attributes}>"
   end
 
-  def wrap_in_tag(tag, attributes, content)
-    "<#{tag}#{attributes}>\n" + indent_lines(content) + "</#{tag}>"
-  end
-
-  def indent_lines(lines)
-    lines.each_line.map{|l| "  " + l }.join
-  end
-
-  def standardise_attributes(element)
-    element[1].is_a?(Hash) ?
-      element : element.insert(1, {})
-  end
-
-  def render_attributes(element)
-    element.shift.map do |key, value|
-      " #{key}=\"#{value}\""
+  def content
+    "\n" +
+    @content.map do |child|
+      "  " + child.rapricot + "\n"
     end.join
   end
 
-  # This allows all methods can be called as Rapricot.method. A class may still
-  # include Rapricot and call the method directly without the namespace prefix.
-  class << self
-    include Rapricot
+  def closing_tag
+    "</#{@tag}>"
+  end
+
+  def attributes
+    @attributes.map do |key, value|
+      " #{key}=\"#{value}\""
+    end.join
   end
 end
 
+class Array
+  def rapricot
+    Tag.new(rapricot_standardise(self)).render
+  end
+
+  private
+    def rapricot_standardise(element)
+      element[1].is_a?(Hash) ?
+        element : element.insert(1, {})
+    end
+end
+
+class String
+  def rapricot
+    self
+  end
+end
+
+[:p, {class: "foo"}, "stuff", "things"].rapricot
--- spec/lib/rapricot_spec.rb
+++ spec/lib/rapricot_spec.rb
@@ -1,19 +1,18 @@
 require 'spec_helper'
 
-describe Rapricot do
-	include Rapricot
-	it "should render void elements" do
-		render([:img, {src: "a.img"}]).should == "<img src=\"a.img\">"
-	end
-	it "should render normal elements" do
-		render([:html]).gsub("\n","").should == "<html></html>"
-	end
-	it "should render nested elements" do
-		# why are there extra spaces?
-		render([:html, [:head]]).gsub("\n", "").should == "<html>  <head>  </head></html>"
-	end
-	it "should render elements correctly" do
-		#should the escape characters be shown in output?
-		render([:input, {type: "text", name: "email", value: "email"}]).should == "<input type=\"text\" name=\"email\" value=\"email\">"
-	end
+describe "rapricot" do
+  it "should render void elements" do
+    [:img, {src: "a.img"}].rapricot.should == "<img src=\"a.img\">"
+  end
+  it "should render normal elements" do
+    [:html].rapricot.gsub("\n","").should == "<html></html>"
+  end
+  it "should render nested elements" do
+    # why are there extra spaces?
+    [:html, [:head]].rapricot.gsub("\n", "").should == "<html>  <head></head></html>"
+  end
+  it "should render elements correctly" do
+    #should the escape characters be shown in output?
+    [:input, {type: "text", name: "email", value: "email"}].rapricot.should == "<input type=\"text\" name=\"email\" value=\"email\">"
+  end
 end
